3d6b509daf40519522fe506eb1f9942827397555
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-08-30 13:39:23 +0200

    Create basic CHIP8 properties and proper unit test for it

--- Sources/CHIP8/CHIP8.swift
+++ Sources/CHIP8/CHIP8.swift
@@ -1,3 +1,19 @@
+
+private enum Constants {
+    static let memSize = 4096
+    static let regs = 16
+    static let stackSize = 16
+}
+
 struct CHIP8 {
-    var text = "Hello, World!"
+    var mem: [UInt8] = [UInt8](repeating: .zero, count: Constants.memSize) // Available Memory
+    var pc: UInt16 = .zero // Program Counter
+    
+    var stack: [UInt16] = [UInt16](repeating: .zero, count: Constants.stackSize) // Stack Memory
+    var sp: UInt16 = .zero // Stack Pointer
+    
+    var v: [UInt8] = [UInt8](repeating: .zero, count: Constants.regs) // Available Registers
+    var i: UInt16 = .zero // Instruction register
+    var dt: UInt8 = .zero // Delay Timer
+    var st: UInt8 = .zero // Sound Timer
 }
--- Tests/CHIP8Tests/CHIP8Tests.swift
+++ Tests/CHIP8Tests/CHIP8Tests.swift
@@ -2,14 +2,40 @@ import XCTest
 @testable import CHIP8
 
 final class CHIP8Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CHIP8().text, "Hello, World!")
+    let chip8 = CHIP8()
+    
+    func testMemSize() {
+        XCTAssert(chip8.mem.count == 4096, "The memory size must be 64 and it is \(chip8.mem.count)")
+        XCTAssert((chip8.mem as Any) is [UInt8], "The memory array is not UInt8")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    func testProgramCounter() {
+        XCTAssert((chip8.pc as Any) is UInt16, "The program counter is not UInt16")
+    }
+    
+    func testStackSize() {
+        XCTAssert(chip8.stack.count == 16, "The stack size must be 16 and it is \(chip8.stack.count)")
+        XCTAssert((chip8.stack as Any) is [UInt16], "The memory array is not UInt16")
+    }
+    
+    func testStackPointer() {
+        XCTAssert((chip8.sp as Any) is UInt16, "The program counter is not UInt16")
+    }
+    
+    func testRegisterSize() {
+        XCTAssert(chip8.v.count == 16, "The register size must be 64 and it is \(chip8.v.count)")
+        XCTAssert((chip8.v as Any) is [UInt8], "The memory array is not UInt8")
+    }
+    
+    func testIntructionRegister() {
+        XCTAssert((chip8.i as Any) is UInt16, "The instruction register is not UInt16")
+    }
+    
+    func testDelayTimer() {
+        XCTAssert((chip8.dt as Any) is UInt8, "The instruction register is not UInt8")
+    }
+    
+    func testSoundTimer() {
+        XCTAssert((chip8.st as Any) is UInt8, "The instruction register is not UInt8")
+    }
 }
