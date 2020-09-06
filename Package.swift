15374cff142f03df2a624a0f16c795a7ed8a3efd
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2020-09-06 20:07:37 +0200

    Add SDL package and example

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CHIP8",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ctreffs/SwiftSDL2.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/CHIP8App/main.swift
+++ Sources/CHIP8App/main.swift
@@ -6,5 +6,39 @@
 //
 
 import Foundation
+import SDL2
+import Metal
+import class QuartzCore.CAMetalLayer
 
-print("Hola")
+SDL_Init(SDL_INIT_VIDEO)
+let window = SDL_CreateWindow("CHIP 8 Emulator", Int32(SDL_WINDOWPOS_CENTERED_MASK), Int32(SDL_WINDOWPOS_CENTERED_MASK), 640, 320, SDL_WINDOW_SHOWN.rawValue | SDL_WINDOW_OPENGL.rawValue)
+
+var pixels: UnsafeMutableRawPointer?
+var pitch: Int32 = .zero
+
+let renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED.rawValue)
+let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888.rawValue, Int32(SDL_TEXTUREACCESS_STREAMING.rawValue), 64, 32)
+SDL_LockTexture(texture, nil, &pixels, &pitch)
+
+// We need to specify the type of the array
+let content = pixels?.assumingMemoryBound(to: UInt8.self)
+content?.assign(repeating: 0xFF, count: 32*Int(pitch))
+SDL_UnlockTexture(texture)
+
+
+var ev = SDL_Event()
+var mustQuit: Bool = false
+while !mustQuit {
+    SDL_RenderClear(renderer)
+    SDL_RenderCopy(renderer, texture, nil, nil)
+    SDL_RenderPresent(renderer)
+    SDL_WaitEvent(&ev)
+    if SDL_EventType(ev.type)  == SDL_QUIT {
+        mustQuit = true
+    }
+}
+
+SDL_DestroyRenderer(renderer)
+SDL_DestroyWindow(window)
+SDL_Quit()
+
