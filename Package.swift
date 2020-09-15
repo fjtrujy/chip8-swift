a5ae9c0bef1d5ae4bff72ff84c94686282ca6192
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2020-09-15 17:19:59 +0200

    Export Libraries in package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "CHIP8",
-    dependencies: [
+    products: [
+        .library(
+            name: "CHIP8Emulator",
+            targets: ["CHIP8FE", "CHIP8", "CHIP8Roms"]),
+    ], dependencies: [
         .package(name: "SDL2", url: "https://github.com/ctreffs/SwiftSDL2.git", from: "1.1.0")
     ],
     targets: [
